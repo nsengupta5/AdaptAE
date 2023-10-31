24dbb22de8ae10efa0ddfe17a675b930045701f0
Nikhil Sengupta <nikhil.sengupta10@protonmail.com>
2023-10-31 00:28:36 +0000

    Added documentation and fixed input data dimension error

--- oselm/oselm.py
+++ oselm/oselm.py
@@ -1,9 +1,10 @@
 import torch
 from torch import nn
 from torch.linalg import pinv
+import logging
 
 class OSELM(nn.Module):
-    
+
     def __init__(self, activation_func, loss_func, n_input_nodes, n_hidden_nodes):
         super().__init__()
 
@@ -23,6 +24,7 @@ class OSELM(nn.Module):
             raise ValueError("Loss function not supported")
 
         # Initialize the input weights and bias and make them orthogonal
+        # TODO Need to make alpha and bias orthogonal?
         self.__alpha = nn.Parameter(torch.randn(n_input_nodes, n_hidden_nodes))
         self.__bias = nn.Parameter(torch.randn(n_hidden_nodes))
 
@@ -31,39 +33,83 @@ class OSELM(nn.Module):
         self.__u = nn.Parameter(torch.zeros(n_hidden_nodes, n_hidden_nodes))
         self.__v = nn.Parameter(torch.zeros(n_hidden_nodes, n_input_nodes))
 
+    """
+    Predict the output of the network based on the input data
+    :param test_data: The test data
+    """
     def predict(self, test_data):
         H = self.__activation_func(torch.matmul(test_data, self.__alpha) + self.__bias)
         return torch.matmul(H, self.__beta)
 
+    """
+    Evaluate the network based on the test data and the predicted data
+    :param test_data: The test data
+    :param pred_data: The predicted data
+    """
     def evaluate(self, test_data, pred_data):
+        # Assert that:
+        # 1. The test data and predicted data have the same shape
+        # 2. The test data shape matches the input nodes
+        # 3. The predicted data shape matches the input nodes
+        assert_cond(test_data.shape[0] == pred_data.shape[0], "Test data and predicted data do not have the same shape")
+        assert_cond(test_data.shape[1] == self.__n_input_nodes, "Test data shape does not match the input nodes")
+        assert_cond(pred_data.shape[1] == self.__n_input_nodes, "Predicted data shape does not match the input nodes")
         loss = self.__loss_func(self.predict(test_data), pred_data)
+        # TODO Do we need accuracy?
         accuracy = torch.sum(torch.argmax(self.predict(test_data), dim=1) == torch.argmax(pred_data, dim=1)) / len(pred_data) * 100
         return loss, accuracy
 
+    """
+    Initialize the network based on the input data
+    :param data: The input data for initialization phase
+    """
     def init_phase(self, data):
+        # Assert that the input data shape matches the input nodes
+        assert_cond(data.shape[1] == self.__n_input_nodes, "Input data shape does not match the input nodes")
+        # Assert that the hidden layer shape matches the hidden nodes
         H = self.__activation_func(torch.matmul(data, self.__alpha) + self.__bias)
+        assert_cond(H.shape[1] == self.__n_hidden_nodes, "Hidden layer shape does not match the hidden nodes")
+        assert_cond(H.shape[0] == data.shape[0], "Hidden layer shape does not match number of samples")
+
         H_T = torch.transpose(H, 0, 1)
         self.__p = pinv(torch.matmul(H_T, H))
         pH_T = torch.matmul(self.__p, H_T)
         self.__beta = torch.matmul(pH_T, data)
         return self.__beta
 
+    """
+    Sequentially train the network based on the input data
+    :param data: The input data for sequential training
+    :param mode: The mode of training, either "chunk" or "sample"
+    """
     def seq_phase(self, data, mode):
-        H = torch.transpose(self.__activation_func(torch.matmul(data, self.__alpha) + self.__bias), 0, 1)
-        H_T = torch.transpose(H, 0, 1)
+        # Assert that the hidden layer shape matches the hidden nodes
+        H = self.__activation_func(torch.matmul(data, self.__alpha) + self.__bias)
+
         if mode == "chunk":
+            H_T = torch.transpose(H, 0, 1)
+            assert_cond(H.shape[1] == self.__n_hidden_nodes, "Hidden layer shape does not match the hidden nodes")
+            assert_cond(data.shape[1] == self.__n_input_nodes, "Input data shape does not match the input nodes")
             chunk_size = data.shape[0]
             self.calc_p_chunk(chunk_size, H, H_T)
             self.calc_beta_chunk(data, H, H_T)
         elif mode == "sample":
+            H = H.unsqueeze(1)
+            H_T = torch.transpose(H, 0, 1)
+            assert_cond(H_T.shape[1] == self.__n_hidden_nodes, "Hidden layer shape does not match the hidden nodes")
             self.calc_p_sample(H, H_T)
             self.calc_beta_sample(data, H, H_T)
         else:
             raise ValueError("Mode not supported")
 
         return self.__beta
-    
-    # Calculate the p of the network based on chunk of input data
+
+    """
+    Calculate the p of the network based on chunk of input data
+    :param chunk_size: The size of the chunk
+    :param H: The hidden layer output matrix
+    :param H_T: The transpose of the hidden layer output matrix
+    """
     def calc_p_chunk(self, chunk_size, H, H_T):
         PH_T = torch.matmul(self.__p, H_T)
         I = torch.eye(chunk_size)
@@ -71,44 +117,88 @@ class OSELM(nn.Module):
         HP = torch.matmul(H, self.__p)
         self.__p -= torch.matmul(torch.matmul(PH_T, HPH_T_Inv), HP)
 
-    # Calculate the beta of the network based on chunk of input data
+    """
+    Calculate the beta of the network based on chunk of input data
+    :param chunk: The chunk of input data
+    :param H: The hidden layer output matrix
+    :param H_T: The transpose of the hidden layer output matrix
+    """
     def calc_beta_chunk(self, chunk, H, H_T):
         T = torch.transpose(chunk, 0, 1)
         THB = T - torch.matmul(H, self.__beta)
         self.__beta += torch.matmul(torch.matmul(self.__p, H_T), THB)
 
-    # Calculate the p of the network based on sample of input data
+    """
+    Calculate the p of the network based on sample of input data
+    :param H: The hidden layer output matrix
+    :param H_T: The transpose of the hidden layer output matrix
+    """
     def calc_p_sample(self, H, H_T):
-        PHH_TP = torch.matmul(torch.matmul(torch.matmul(self.__p, H), H_T), self.__p)
+        PH = torch.matmul(self.__p, H)
+        PHH_T = torch.matmul(PH, H_T)
+        PHH_TP = torch.matmul(PHH_T, self.__p)
         H_TPH = torch.matmul(H_T, torch.matmul(self.__p, H))
         self.__p -= torch.div(PHH_TP, 1 + H_TPH)
 
-    # Calculate the beta of the network based on sample of input data
+    """
+    Calculate the beta of the network based on sample of input data
+    :param sample: The sample of input data
+    :param H: The hidden layer output matrix
+    """
     def calc_beta_sample(self, sample, H, H_T):
         THB = sample - torch.matmul(H_T, self.__beta)
-        PH = torch.matmul(self.__p, H)
-        self.__beta += torch.matmul(PH, THB)
+        PH_T = torch.matmul(self.__p, H)
+        self.__beta += torch.matmul(PH_T, THB)
 
+    """
+    Calculate the intermediate u of the network as part of E^2LM
+    """
     def calc_interm_u(self):
         self.__u = pinv(self.__p)
 
+    """
+    Calculate the intermediate v of the network as part of E^2LM
+    """
     def calc_interm_v(self):
         self.__v = torch.matmul(self.__u, self.__beta)
 
+    """
+    Integrate the intermediate u and v from another network into this network as part of E^2LM
+    """
     def integrate(self, u, v):
         self.__u += u
         self.__v += v
         self.__p = pinv(self.__u)
         self.__beta = torch.matmul(pinv(self.__u), self.__v)
 
+    """
+    Orthogonalize the factor matrix
+    :param factor: The factor matrix
+    """
     def gram_schmidt(self,factor):
         Q, _ = torch.linalg.qr(factor)
         return Q
 
+    """
+    Return the input shape of the network
+    """
     @property
     def input_shape(self):
         return (self.__n_input_nodes,)
 
+    """
+    Return the hidden shape of the network
+    """
     @property
     def hidden_shape(self):
         return (self.__n_hidden_nodes,)
+
+"""
+Assert a condition and log the error if the condition is not met
+"""
+def assert_cond(condition, msg):
+    try:
+        assert condition, msg
+    except AssertionError as e:
+        logging.error(e)
+        raise e
--- oselm/train-os-elm.py
+++ oselm/train-os-elm.py
@@ -1,18 +1,19 @@
-from oselm import OSELM
+from oselm import OSELM, assert_cond
 from torchvision import datasets, transforms
 from torch.backends import mps
 from torch import cuda
 from torch.utils.data import random_split
+from sys import argv
 import logging
-from torch.profiler import profile, ProfilerActivity
 
+# Constants
 TRAIN_SIZE_PROP = 0.6
 SEQ_SIZE_PROP = 0.2
 TEST_SIZE_PROP = 0.2
 BATCH_SIZE = 64
-
-# Get CPU, GPU or MPS Device for training
-device = (
+INPUT_NODES = 784
+HIDDEN_NODES = 128
+DEVICE = (
     "cuda"
     if cuda.is_available()
     else "mps"
@@ -20,59 +21,102 @@ device = (
     else "cpu"
 )
 
+"""
+Initialize the OSELM model
+"""
 def oselm_init():
     activation_func = 'sigmoid'
     loss_func = 'mse'
-    n_hidden_nodes = 128
-    n_input_nodes = 784
-    return OSELM(activation_func, loss_func, n_input_nodes, n_hidden_nodes).to(device)
+    return OSELM(activation_func, loss_func, INPUT_NODES, HIDDEN_NODES).to(DEVICE)
 
-# Data Loading and Splitting
+"""
+Load and split the MNIST data into training, sequential and test data
+"""
 def load_and_split_data():
     transform = transforms.ToTensor()
     mnist_data = datasets.MNIST(root = './data', train = True, download = True, transform = transform)
+
+    # Split 60% for training, 20% for sequential training and 20% for testing
     train_size = int(0.6 * len(mnist_data))
     seq_size = int(0.2 * len(mnist_data))
     test_size = len(mnist_data) - train_size - seq_size
     train_data, seq_data, test_data = random_split(mnist_data, [train_size, seq_size, test_size])
     return train_data, seq_data, test_data
 
-def train_model(model, train_data, seq_data, mode):
-    logging.basicConfig(level=logging.INFO)
-    data = train_data.dataset.data.view(-1, 784).float().to(device)
+"""
+Initialize the OSELM model with the initial training data
+:param model: The OSELM model
+:param train_data: The initial training data
+"""
+def train_init(model, train_data):
+    # Assert that the initial training data is of the correct shape
+    data = train_data.dataset.data.view(-1, 784).float().to(DEVICE)
+    assert_cond(data.shape[0] == len(train_data.dataset), "Train data shape mismatch")
     logging.info(f"Initial training on {len(data)} samples...")
-    # with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
+    logging.info("Train data shape: " + str(data.shape))
     model.init_phase(data)
-    # print(prof.key_averages().table(sort_by="cuda_time_total"))
     logging.info(f"Initial training complete.")
 
-    logging.info(f"Sequential training on {len(seq_data.dataset)} samples...")
+"""
+Train the OSELM model sequentially on the sequential training data
+:param model: The OSELM model
+:param seq_data: The sequential training data
+:param mode: The mode of sequential training, either "sample" or "batch"
+"""
+def train_sequential(model, seq_data, mode):
+    logging.info(f"Sequential training on {len(seq_data.dataset)} samples in {mode} mode...")
+    data = seq_data.dataset.data.view(-1, 784).float().to(DEVICE)
+    assert_cond(data.shape[0] == len(seq_data.dataset), "Sequential data shape mismatch")
+    logging.info("Sequential data shape: " + str(data.shape))
     if mode == "sample":
-        # with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
-        for i in range(20000):
-            image, _ = seq_data.dataset[i]
-            sample = image.view(-1, 784).float().to(device)
-            model.seq_phase(sample, mode)
-        # print(prof.key_averages().table(sort_by="cuda_time_total"))
+        for image in data:
+            model.seq_phase(image, mode)
     else:
-        for i in range(0, len(seq_data.dataset), BATCH_SIZE):
-            images, _ = seq_data.dataset[0][i:i+BATCH_SIZE]
-            batch = images.view(-1, 784).float().to(device)
-            model.seq_phase(batch, mode)
+        for i in range(0, len(data), BATCH_SIZE):
+            images = seq_data.dataset[i:i+BATCH_SIZE]
+            model.seq_phase(images, mode)
     logging.info(f"Sequential training complete.")
 
+"""
+Test the OSELM model on the test data
+:param model: The OSELM model
+:param test_data: The test data
+"""
 def test_model(model, test_data):
-    data = test_data.dataset.data.view(-1, 784).float().to(device)
-    pred = model.predict(data)
-    loss, accuracy = model.evaluate(data, pred)
-    print(f"Loss: {loss.item():.2f}")
-    print(f"Accuracy: {accuracy.item():.2f}%")
+    logging.info(f"Testing on {len(test_data.dataset)} samples...")
+    data = test_data.dataset.data.view(-1, 784).float().to(DEVICE)
+    assert_cond(data.shape[0] == len(test_data.dataset), "Test data shape mismatch")
+    for i in range(0, len(data), BATCH_SIZE):
+        images = test_data.dataset[i:i+BATCH_SIZE]
+        pred = model.predict(images)
+        loss, accuracy = model.evaluate(images, pred)
+        print(f"Loss: {loss.item():.2f}")
+        print(f"Accuracy: {accuracy.item():.2f}%")
+
+def exit_with_error():
+    print("Usage: train-os-elm.py <mode>")
+    print("mode: sample or batch")
+    exit(1)
 
 def main():
+    # Parse command line arguments
+    if len(argv) == 1:
+        # Default to sample mode
+        mode = "sample"
+    elif len(argv) == 2:
+        if argv[1] not in ["sample", "batch"]:
+            exit_with_error()
+        else:
+            mode = argv[1]
+    else:
+        exit_with_error()
+
+    logging.basicConfig(level=logging.INFO)
     train_data, seq_data, test_data = load_and_split_data()
     model = oselm_init()
-    train_model(model, train_data, seq_data, mode = "sample")
+    train_init(model, train_data)
+    train_sequential(model, seq_data, mode)
     test_model(model, test_data)
-    
+
 if __name__ == "__main__":
     main()
