9e45c38df575c7c887807b0aac5c897630863d02
Nikhil Sengupta <nikhil.sengupta10@protonmail.com>
2024-01-06 00:31:24 +0800

    Removed unnecessary files

--- run.sh
+++
@@ -1,5 +0,0 @@
-python autoencoder/train-autoencoder.py --dataset mnist --task anomaly-detection 
-python autoencoder/train-autoencoder.py --dataset fashion-mnist --task anomaly-detection  
-python autoencoder/train-autoencoder.py --dataset cifar10 --task anomaly-detection 
-python autoencoder/train-autoencoder.py --dataset cifar100 --task anomaly-detection  
-python autoencoder/train-autoencoder.py --dataset super-tiny-imagenet --task anomaly-detection 
